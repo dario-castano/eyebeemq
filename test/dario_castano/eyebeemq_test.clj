16fe7cca3fa99f60ab1ac4e56bf9a8ea895832ea
Dario Castaño <dario.castano@aim.com>
2023-03-14 21:22:24 -0500

    Initial commit

---
+++ test/dario_castano/eyebeemq_test.clj
@@ -0,0 +1,7 @@
+(ns dario-castano.eyebeemq-test
+  (:require [clojure.test :refer :all]
+            [dario-castano.eyebeemq :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
